c34cf9bbe562a8f83df105925f7eb8cab1c7631d
Enron Hubbard <tygerhardt@gmail.com>
2020-10-17 16:42:27 -0700

    Update connection.js

--- src/config/connection.js
+++ src/config/connection.js
@@ -1,19 +1,20 @@
 import mysql from "mysql";
+var mysql = require('mysql');
+var connection;
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "wheat469",
-  database: "moodscape_db"
-});
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
 
-connection.connect(function(err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
-  console.log("connected as id " + connection.threadId);
-});
+} else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "Gerhardtwashere68!",
+    database: "moodscape_db"
+  });
+};
+
+
 
 module.exports = connection;
